617e8d3dfd3de5bf1f473bc4b03dc0c449c6279a
usuario <usuario@JULIO-CAJAMARCA>
2024-04-08 13:04:31 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace mchuquinT1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace mchuquinT1
+{
+    public partial class MainPage : ContentPage
+    {
+       public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btnCalculo_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            int edad = Convert.ToInt32(txtEdad.Text);
+            double sueldo = Convert.ToDouble(txtSueldo.Text);
+            double aporte = (sueldo * 9.45) / 100;
+            DisplayAlert("ALERTA", "Bienvenido " + nombre + " " + apellido + "\n Tienes " + edad + " años " + "\n Tu aporte es de " + aporte, "Cerrar");
+
+
+        }
+    }
+
+}
